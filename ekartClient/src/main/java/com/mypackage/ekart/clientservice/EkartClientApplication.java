61c32e958cf9e16b95c9e1954106faa33d5bd927
Ramesh Singh <rs376q@att.com>
2018-03-25 22:12:46 -0500

    Added a ekart client microservice which uses DB service.

---
+++ ekartClient/src/main/java/com/mypackage/ekart/clientservice/EkartClientApplication.java
@@ -0,0 +1,12 @@
+package com.mypackage.ekart.clientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkartClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkartClientApplication.class, args);
+	}
+}
---
+++ ekartClient/src/main/java/com/mypackage/ekart/clientservice/resource/Customer.java
@@ -0,0 +1,28 @@
+package com.mypackage.ekart.clientservice.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(value= "/rest/ekart/customers")
+public class Customer {
+
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping(value="/{customerId}")
+	public Object getCustomerById(@PathVariable Long customerId){
+		return restTemplate.exchange("http://localhost:8300/rest/db/customers/"+customerId,
+				HttpMethod.GET,
+				null, new ParameterizedTypeReference<Object>() {
+		}).getBody();
+
+	}
+}
+

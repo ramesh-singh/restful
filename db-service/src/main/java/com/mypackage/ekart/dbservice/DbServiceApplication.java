32102b96688db52345836b8cb975f4348104bbb9
Ramesh Singh <rs376q@att.com>
2018-03-25 22:42:21 -0500

    Registered the two micro services on eureka server.

--- db-service/src/main/java/com/mypackage/ekart/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/mypackage/ekart/dbservice/DbServiceApplication.java
@@ -3,8 +3,10 @@ package com.mypackage.ekart.dbservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @EnableCaching
 @EnableJpaRepositories(basePackages="com.mypackage.ekart.dbservice.repository")
 @SpringBootApplication

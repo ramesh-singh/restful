52d2644dc099e43f68f65963780ea4f892756438
Ramesh Singh <rs376q@att.com>
2018-03-28 21:38:16 -0500

    Added code to create API Gateway using netflix Zull proxy.

--- eurekaServer/src/main/java/com/mypackage/ekart/eureka/server/EurekaServerApplication.java
+++ eurekaServer/src/main/java/com/mypackage/ekart/eureka/server/EurekaServerApplication.java
@@ -3,7 +3,10 @@ package com.mypackage.ekart.eureka.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+@EnableZuulProxy
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
